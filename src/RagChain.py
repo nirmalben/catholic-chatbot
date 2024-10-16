cdaf20673185222bf44fd2fa0335a93ddc0aee8c
Nirmal Benann <njbenann@gmail.com>
2024-10-15 23:37:11 -0700

    Update RAG Chain builder to use ChromaDB

--- src/RagChain.py
+++ src/RagChain.py
@@ -1,22 +1,19 @@
+from ChromaVectorStorage import ChromaVectorStorage
 from dotenv import load_dotenv
 from JsonDocumentLoader import JsonDocumentLoader
 from langchain_community.embeddings import HuggingFaceInferenceAPIEmbeddings
 from langchain_core.documents.base import Document
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_pinecone import PineconeVectorStore
-from pinecone import Pinecone, ServerlessSpec
 from typing import List
+
 
 import itertools
 import os
-
-PINECONE_INDEX_NAME = "catholic-chatbot-index"
 
 class RagChain():
   def __init__(self) -> None:
     load_dotenv()
     self._HUGGING_FACE_API_KEY = os.getenv('HUGGING_FACE_API_KEY')
-    self._PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
     self._build()
 
   def _load_bible(self) -> List[Document]:
@@ -48,44 +45,34 @@ class RagChain():
     return list(itertools.chain(*documents)) # converts List[List[Documents]] to List[Documents]
 
   def _split_to_chunks(self, documents):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1600, chunk_overlap=200)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=384, chunk_overlap=100)
     chunks = text_splitter.split_documents(documents)
     return chunks
-  
-  def _get_vector_store(self, chunks, embeddings):
-    pc = Pinecone(api_key= os.getenv('PINECONE_API_KEY'))
-    existing_indexes = [index_info["name"] for index_info in pc.list_indexes()]
-
-    is_index_new = False
-    if PINECONE_INDEX_NAME not in existing_indexes:
-      is_index_new = True
-      pc.create_index(
-        name=PINECONE_INDEX_NAME,
-        dimension=1024,
-        spec=ServerlessSpec(cloud="aws", region="us-east-1"),
-        metric="cosine")
-    index = pc.Index(PINECONE_INDEX_NAME)
-    
-    if is_index_new:
-      PineconeVectorStore.from_documents(chunks, embeddings, index_name=PINECONE_INDEX_NAME)
-
-    return PineconeVectorStore(index=index, embedding=embeddings)
 
   def _build(self):
     # Load documents
+    print("Loading Documents...")
     documents = self._load_documents()
     
     # Split documents into chunks
+    print("Splitting documents into chunks...")
     chunks = self._split_to_chunks(documents=documents)
     
     # Initialize embeddings
+    print("Initialize embeddings...")
     embeddings = HuggingFaceInferenceAPIEmbeddings(
       api_key=self._HUGGING_FACE_API_KEY, 
-      model_name="thenlper/gte-large") # https://huggingface.co/thenlper/gte-large
-
-    # Vectorstore for the chunks
-    vector_store = self._get_vector_store(chunks=chunks, embeddings=embeddings)
+      model_name="sentence-transformers/all-MiniLM-L6-v2") # https://huggingface.co/sentence-transformers/all-MiniLM-L6-v2
     
+    # Vectorstore for the chunks
+    print("Initialize vector store...")
+    vector_store = ChromaVectorStorage(
+      chunks=chunks,
+      embeddings=embeddings).get_vector_store()
+
+    print("Perform similarity search...")
+    print(vector_store.similarity_search("Mother Mary is our intercessor."))
+
     # Retriever
     
     # Memory
