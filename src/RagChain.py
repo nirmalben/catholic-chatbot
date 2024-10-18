228db75e8d7c1f53cbf160e7c531875a2ac38716
Nirmal Benann <njbenann@gmail.com>
2024-10-18 01:46:30 -0700

    Use HuggingFaceEndpoint, comment print statements and update prompts

--- src/RagChain.py
+++ src/RagChain.py
@@ -6,6 +6,7 @@ from langchain.chains.conversational_retrieval.base import ConversationalRetriev
 from langchain_community.embeddings import HuggingFaceInferenceAPIEmbeddings
 from langchain_community.llms.huggingface_hub import HuggingFaceHub
 from langchain_core.documents.base import Document
+from langchain_huggingface import HuggingFaceEndpoint
 from langchain.memory import ConversationBufferMemory
 from langchain.prompts import PromptTemplate, ChatPromptTemplate
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -14,6 +15,8 @@ from typing import List
 
 import itertools
 import os
+
+LLM_NAME = 'mistralai/Mixtral-8x7B-Instruct-v0.1' # https://huggingface.co/mistralai/Mixtral-8x7B-Instruct-v0.1
 
 class RagChain():
   def __init__(self) -> None:
@@ -65,22 +68,18 @@ class RagChain():
     return chunks
 
   def _build(self):
-    # Load documents
-    print("Loading Documents...")
+    # print("Loading Documents...")
     documents = self._load_documents()
     
-    # Split documents into chunks
-    print("Splitting documents into chunks...")
+    # print("Splitting documents into chunks...")
     chunks = self._split_to_chunks(documents=documents)
     
-    # Initialize embeddings
-    print("Initialize embeddings...")
+    # print("Initialize embeddings...")
     embeddings = HuggingFaceInferenceAPIEmbeddings(
       api_key=self._HUGGING_FACE_API_KEY, 
       model_name="sentence-transformers/all-MiniLM-L6-v2") # https://huggingface.co/sentence-transformers/all-MiniLM-L6-v2
     
-    # Vectorstore for the chunks
-    print("Initialize vector store...")
+    # print("Initialize vectorstore...")
     vector_store = ChromaVectorStorage(chunks=chunks, embeddings=embeddings).get_vector_store()
 
     # print("Perform similarity search...")
@@ -100,45 +99,41 @@ class RagChain():
       Follow Up Input: {question}\n
       Standalone question:""")
     
-    answer_template = """You are an expert that is able to look up all information on Catholicism and offer tips on using Catholicism in life. You have been asked the following question and if the question pertains to something that can answered with Catholicism, using the following context, you can answer it. If the question is generic, does not pertain to Catholicism or does not relate to Catholicism, you can say that the question cannot be answered with Catholicism.\n\n
+    answer_template = """You are an expert in only offering tips about life using Catholicism and also answer questions on Catholicism. You have been asked the following question and if the question pertains to something that can answered with Catholicism only, using the following context, you can answer it. If the question is generic, does not pertain to Catholicism or does not relate to Catholicism, you can say that the question cannot be answered with Catholicism. If there is no question, you can respond accordingly\n\n
     Chat History:\n{chat_history}\n
     Context:\n{context}\n
     
-    Question:{question}\n
+    Question: {question}\n
     Answer:"""
     answer_prompt = ChatPromptTemplate.from_template(answer_template)
 
-    standalone_query_llm = HuggingFaceHub(
-      repo_id='mistralai/Mixtral-8x7B-Instruct-v0.1',
+    standalone_query_llm = HuggingFaceEndpoint(
+      repo_id=LLM_NAME,
       huggingfacehub_api_token=self._HUGGING_FACE_API_KEY,
-      model_kwargs={
-        "temperature": 0.1,
-        "top_p": 0.95,
-        "do_sample": True,
-        "max_new_tokens": 1024,
-      },
+      temperature=0.1,
+      top_p=0.95,
+      do_sample=True,
+      max_new_tokens=1024
     )
-    response_llm = HuggingFaceHub(
-      repo_id='mistralai/Mixtral-8x7B-Instruct-v0.1',
+    response_llm = HuggingFaceEndpoint(
+      repo_id=LLM_NAME,
       huggingfacehub_api_token=self._HUGGING_FACE_API_KEY,
-      model_kwargs={
-        "temperature": 0.1,
-        "top_p": 0.95,
-        "do_sample": True,
-        "max_new_tokens": 1024,
-      },
+      temperature=0.1,
+      top_p=0.95,
+      do_sample=True,
+      max_new_tokens=1024
     )
 
     self.chain = ConversationalRetrievalChain.from_llm(
-        condense_question_prompt=condense_question_prompt,
-        combine_docs_chain_kwargs={"prompt": answer_prompt},
-        condense_question_llm=standalone_query_llm,
-        llm=response_llm,
-        memory=self.memory,
-        retriever=retriever,
-        chain_type="stuff",
-        verbose=False,
-        return_source_documents=True)
+      condense_question_prompt=condense_question_prompt,
+      combine_docs_chain_kwargs={"prompt": answer_prompt},
+      condense_question_llm=standalone_query_llm,
+      llm=response_llm,
+      memory=self.memory,
+      retriever=retriever,
+      chain_type="stuff",
+      verbose=False,
+      return_source_documents=True)
   
   def get_chain(self):
     return self.chain
